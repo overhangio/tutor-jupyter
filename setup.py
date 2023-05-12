0f3137c70c5204927cca76f8bb87e91f2374c8c9
Régis Behmo <regis@behmo.com>
2023-05-12 17:27:47 +0200

    fix: package depends on mfe

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor>=15.0.0,<16.0.0"],
+    install_requires=["tutor>=15.0.0,<16.0.0", "tutor-mfe>=15.0.0,<16.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "jupyter = tutorjupyter.plugin"
