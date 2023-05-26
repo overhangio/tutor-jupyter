bf17765718e393cd4be1c115bf10025976b5e5f8
Régis Behmo <regis@behmo.com>
2023-05-26 18:47:48 +0200

    fix: nightly package version
    The package version number may not include the "-nightly" suffix.
    Otherwise, installation fails with:
    
       setuptools.extern.packaging.version.InvalidVersion: Invalid version: '15.0.7-nightly'

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ABOUT = load_about()
 
 setup(
     name="tutor-jupyter",
-    version=ABOUT["__version__"],
+    version=ABOUT["__package_version__"],
     url="https://github.com/overhangio/tutor-jupyter",
     project_urls={
         "Code": "https://github.com/overhangio/tutor-jupyter",
